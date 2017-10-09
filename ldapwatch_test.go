7943690e800790650035de3ae3469cdb89bacab7
Matt Todd <chiology@gmail.com>
2017-10-09 01:08:21 -0400

    Start testing ldapwatch

---
+++ ldapwatch_test.go
@@ -0,0 +1,165 @@
+package ldapwatch
+
+import (
+	"fmt"
+	"log"
+	"testing"
+
+	ldap "gopkg.in/ldap.v2"
+)
+
+const (
+	host = "localhost"
+	port = 389
+
+	bindusername = "cn=admin,dc=planetexpress,dc=com"
+	bindpassword = "GoodNewsEveryone"
+
+	base = "ou=people,dc=planetexpress,dc=com"
+)
+
+func TestEnv(t *testing.T) {
+	conn, err := ldap.Dial("tcp", fmt.Sprintf("%s:%d", host, port))
+	if err != nil {
+		t.Fatalf("ldap.Dial: %s", err)
+	}
+	defer conn.Close()
+
+	err = conn.Bind(bindusername, bindpassword)
+	if err != nil {
+		t.Fatalf("ldap.Bind: %s", err)
+	}
+}
+
+func TestWatchPerson(t *testing.T) {
+	conn, err := ldap.Dial("tcp", fmt.Sprintf("%s:%d", host, port))
+	if err != nil {
+		t.Fatalf("ldap.Dial: %s", err)
+	}
+	defer conn.Close()
+
+	err = conn.Bind(bindusername, bindpassword)
+	if err != nil {
+		t.Fatalf("ldap.Bind: %s", err)
+	}
+
+	// Search for the given username
+	searchRequest := ldap.NewSearchRequest(
+		base,
+		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
+		// fmt.Sprintf("(&(objectClass=organizationalPerson)&(uid=%s))", username),
+		fmt.Sprintf("(uid=%s)", "fry"),
+		[]string{"dn", "modifyTimestamp"},
+		nil,
+	)
+
+	// FIXME: duplicate a person that we can modify
+
+	updated := false
+	updates := make(chan Result)
+	done := make(chan struct{})
+	defer func() { close(done) }()
+	go func(c chan Result, done chan struct{}) {
+		for {
+			select {
+			case <-c:
+				updated = true
+			case <-done:
+				return
+			}
+		}
+	}(updates, done)
+
+	compare := func(prev Result, next Result) bool {
+		// no previous results (initial search); treat as unchanged
+		if (Result{}) == prev {
+			return false
+		}
+
+		// check length differences
+		if len(prev.Results.Entries) != len(next.Results.Entries) {
+			return true
+		}
+
+		prevE := prev.Results.Entries[0]
+		nextE := next.Results.Entries[0]
+		if prevE.DN != nextE.DN {
+			// log.Println(fmt.Sprintf("DN mismatch %#v %#v", prevE, nextE))
+			return true
+		}
+
+		prevMod := nextE.GetAttributeValue("modified")
+		nextMod := nextE.GetAttributeValue("modified")
+		if prevMod != nextMod {
+			// log.Println(fmt.Sprintf("modified mismatch %#v %#v", prevMod, nextMod))
+			return true
+		}
+
+		return false
+	}
+
+	t.Run("unmodified", func(t *testing.T) {
+		updated = false
+
+		watcher, err := NewWatcher(conn)
+		if err != nil {
+			t.Fatalf("NewWatcher: %s", err)
+		}
+
+		err = watcher.Add(searchRequest, compare, updates)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// first run, nothing expected
+		search(watcher)
+
+		if updated {
+			t.Fatalf("entry was marked as updated on the first round")
+		}
+
+		// second run, nothing expected
+		search(watcher)
+
+		if updated {
+			t.Fatalf("entry was marked as updated but should've been unchanged")
+		}
+	})
+
+	// kill the update gofunc
+	t.Run("modified", func(t *testing.T) {
+		updated = false
+
+		watcher, err := NewWatcher(conn)
+		if err != nil {
+			t.Fatalf("NewWatcher: %s", err)
+		}
+
+		err = watcher.Add(searchRequest, compare, updates)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// first run, nothing expected
+		search(watcher)
+
+		if updated {
+			t.Fatalf("entry was marked as updated on the first round")
+		}
+
+		// second run, nothing expected
+		search(watcher)
+
+		if !updated {
+			t.Fatalf("entry was not marked as updated but should've been")
+		}
+	})
+}
+
+func TestWatchPeople(t *testing.T) {
+	// t.Errorf("because")
+}
+
+func TestWatchGroup(t *testing.T) {
+	// t.Errorf("because")
+}
