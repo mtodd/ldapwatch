8e8a6b23e3f3d5e613d8d26659ea31dbbad59ea7
Matt Todd <chiology@gmail.com>
2018-01-28 16:20:23 -0500

    Test modification

--- ldapwatch_test.go
+++ ldapwatch_test.go
@@ -6,7 +6,9 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"testing"
+	"time"
 
 	ldap "gopkg.in/ldap.v2"
 )
@@ -23,6 +25,8 @@ const (
 
 var (
 	network string
+
+	updated bool
 )
 
 func TestMain(m *testing.M) {
@@ -55,6 +59,75 @@ func TestEnv(t *testing.T) {
 	}
 }
 
+func findEntry(c *ldap.Conn, cn string) (*ldap.Entry, error) {
+	sr := ldap.NewSearchRequest(
+		base,
+		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
+		fmt.Sprintf("(cn=%s)", cn),
+		nil,
+		nil,
+	)
+	res, err := c.Search(sr)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(res.Entries) != 1 {
+		return nil, fmt.Errorf("one result expected, %d found", len(res.Entries))
+	}
+
+	return res.Entries[0], nil
+}
+
+func clearDupes(c *ldap.Conn, dupeRdn string) error {
+	sr := ldap.NewSearchRequest(
+		base,
+		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
+		fmt.Sprintf("(cn=copy-%s)", dupeRdn),
+		nil,
+		nil,
+	)
+	res, err := c.Search(sr)
+	if err != nil {
+		return err
+	}
+
+	if len(res.Entries) > 0 {
+		for _, entry := range res.Entries {
+			dr := ldap.NewDelRequest(entry.DN, nil)
+			if err := c.Del(dr); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func dupEntry(c *ldap.Conn, existingRdn string) error {
+	entry, err := findEntry(c, existingRdn)
+	if err != nil {
+		return err
+	}
+
+	dupeDn := strings.Replace(entry.DN, existingRdn, "copy-"+existingRdn, 1)
+	ar := ldap.NewAddRequest(dupeDn)
+
+	// copy attributes
+	for _, entryAttr := range entry.Attributes {
+		switch entryAttr.Name {
+		case "uid":
+			fallthrough
+		case "cn":
+			ar.Attribute(entryAttr.Name, []string{"copy-" + entry.GetAttributeValue(entryAttr.Name)})
+		default:
+			ar.Attribute(entryAttr.Name, entryAttr.Values)
+		}
+	}
+
+	return c.Add(ar)
+}
+
 func TestWatchPerson(t *testing.T) {
 	conn, err := ldap.Dial("tcp", network)
 	if err != nil {
@@ -67,19 +140,23 @@ func TestWatchPerson(t *testing.T) {
 		t.Fatalf("ldap.Bind: %s", err)
 	}
 
+	if err = clearDupes(conn, "Philip J. Fry"); err != nil {
+		t.Fatalf("clearDupes: %s", err)
+	}
+	if err = dupEntry(conn, "Philip J. Fry"); err != nil {
+		t.Fatalf("dupEntry: %s", err)
+	}
+
 	// Search for the given username
 	searchRequest := ldap.NewSearchRequest(
 		base,
 		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
 		// fmt.Sprintf("(&(objectClass=organizationalPerson)&(uid=%s))", username),
-		fmt.Sprintf("(uid=%s)", "fry"),
-		[]string{"dn", "modifyTimestamp"},
+		fmt.Sprintf("(cn=%s)", "copy-Philip J. Fry"),
+		[]string{"dn", "mail", "modifyTimestamp"},
 		nil,
 	)
 
-	// FIXME: duplicate a person that we can modify
-
-	updated := false
 	updates := make(chan Result)
 	done := make(chan struct{})
 	defer func() { close(done) }()
@@ -112,10 +189,18 @@ func TestWatchPerson(t *testing.T) {
 			return true
 		}
 
-		prevMod := nextE.GetAttributeValue("modified")
-		nextMod := nextE.GetAttributeValue("modified")
+		prevMod := prevE.GetAttributeValue("modifyTimestamp")
+		nextMod := nextE.GetAttributeValue("modifyTimestamp")
 		if prevMod != nextMod {
-			// log.Println(fmt.Sprintf("modified mismatch %#v %#v", prevMod, nextMod))
+			// log.Println(fmt.Sprintf("modifyTimestamp mismatch %#v %#v", prevMod, nextMod))
+			return true
+		}
+
+		// fallback comparison when modifyTimestamp won't reflect subsecond updates
+		prevMail := prevE.GetAttributeValue("mail")
+		nextMail := nextE.GetAttributeValue("mail")
+		if prevMail != nextMail {
+			// log.Println(fmt.Sprintf("mail mismatch %#v %#v", prevMail, nextMail))
 			return true
 		}
 
@@ -171,8 +256,22 @@ func TestWatchPerson(t *testing.T) {
 			t.Fatalf("entry was marked as updated on the first round")
 		}
 
+		time.Sleep(1 * time.Second)
+
+		entry, err := findEntry(conn, "copy-Philip J. Fry")
+		if err != nil {
+			t.Fatalf("%s", err)
+		}
+		mr := ldap.NewModifyRequest(entry.DN)
+		mr.Replace("mail", []string{"fired@example.org"})
+		if err = conn.Modify(mr); err != nil {
+			t.Fatalf("modify failed: %s", err)
+		}
+
 		// second run, nothing expected
 		search(watcher)
+
+		time.Sleep(100 * time.Millisecond)
 
 		if !updated {
 			t.Fatalf("entry was not marked as updated but should've been")
